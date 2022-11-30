66efca31a1d0e086d98e61015f5f2e4429b675a1
Luan <luaninfo09@gmail.com>
2022-11-30 10:03:53 -0300

    Criação da classe email

--- envio.php
+++ envio.php
@@ -1,2 +1,59 @@
 <?php
 print_r($_POST);
+
+
+class Email
+{
+
+    private $destino = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+    public function __construct($destino, $assunto, $mensagem)
+    {
+
+        $this->destino = $destino;
+        $this->assunto = $assunto;
+        $this->mensagem = $mensagem;
+    }
+
+
+    public function __get($name)
+    {
+
+        return $this->$name;
+    }
+
+    public function __set($name, $value)
+    {
+
+        $this->$name = $value;
+    }
+
+    public function envioValido()
+    {
+
+        if (empty($this->destino) || empty($this->assunto) || empty($this->mensagem)) {
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+$email = new Email($_POST['destino'], $_POST['assunto'], $_POST['mensagem']);
+
+// $mensagem->__set('destino', $_POST['destino']);
+// $mensagem->__set('assunto', $_POST['assunto']);
+// $mensagem->__set('mensagem', $_POST['mensagem']);
+
+// print_r($email);
+
+if ($email->envioValido()) {
+
+    echo 'mensagem valida';
+} else {
+
+    echo 'mensagem não valida';
+}
