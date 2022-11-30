6648a3f8a30e9c067f47256772fd38350f263b49
Luan <luaninfo09@gmail.com>
2022-11-30 09:20:02 -0300

    Criação do formulario

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+
+				<div class="card-body font-weight-bold">
+					<form>
+						<div class="form-group">
+							<label for="para">Para</label>
+							<input type="text" class="form-control" id="para" placeholder="usuario@mail.com">
+						</div>
+
+						<div class="form-group">
+							<label for="assunto">Assunto</label>
+							<input type="text" class="form-control" id="assunto" s>
+						</div>
+
+						<div class="form-group">
+							<label for="mensagem">Mensagem</label>
+							<textarea class="form-control" id="mensagem"></textarea>
+						</div>
+
+						<button type="submit" class="btn btn-secondary btn-lg">Enviar Mensagem</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</html>
